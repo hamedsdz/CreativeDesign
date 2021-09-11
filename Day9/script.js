31579b97f46c9c3555ca3379a883d757a33a2055
Hamed Sadeghzadeh <hsadeghzadeh79@gmail.com>
2021-09-11 20:52:19 +0430

    Finish Day 9 ( Sound Board )

---
+++ Day9/script.js
@@ -0,0 +1,20 @@
+const sounds = ['Bass', 'Game', 'Urgent', 'Alarm']
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+  btn.innerText = sound
+  btn.addEventListener('click', () => {
+    stopSounds()
+    document.getElementById(sound).play()
+  })
+  document.getElementById('btnContainer').appendChild(btn)
+})
+
+function stopSounds() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound)
+    song.pause()
+    song.currentTime = 0
+  })
+}
